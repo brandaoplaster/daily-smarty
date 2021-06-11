fe2d7f8f1d33972e786c3900df7a9fac28fc6a4c
brandaoplaster <brandaoplaster@gmail.com>
2021-06-10 22:23:37 -0300

    feat: Adding model user and tests to the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  after_initialize :set_defaults
+
+  private
+
+  def set_defaults
+    self.role ||= 'student'
+  end
 end
