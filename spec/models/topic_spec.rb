6fb461abe40567a502e8c15b350c525c37219fdb
brandaoplaster <brandaoplaster@gmail.com>
2021-06-11 23:43:10 -0300

    feat: Validating model topic and adding tests

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @topic = Topic.create(title: "front end")
+  end
+
+  describe 'validations' do
+    it 'cannot be created without a title' do
+      @topic.title = nil
+      expect(@topic).to_not be_valid
+    end
+
+    xit 'cannot be created without a slug' do
+      @topic.slug = nil
+      expect(@topic).to_not be_valid
+    end
+  end
+
+  describe 'callbacks' do
+    it 'automatically sets the slug value event with a nil value submit' do
+      expect(@topic.slug).to_not eq(nil)
+    end
+  end
 end
