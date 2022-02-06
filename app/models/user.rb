4d35a713ff59687e743f36cca47898fbb91906b1
brandaoplaster <brandaoplaster@gmail.com>
2022-02-06 15:56:45 -0300

    feat: Creating resource post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
   after_initialize :set_defaults
+
+  has_many :posts
 
   validates_presence_of :first_name, :last_name
   validates :username, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z]+([a-zA-Z]|\d)*\Z/ }
@@ -13,6 +12,6 @@ class User < ApplicationRecord
   private
 
   def set_defaults
-    self.role ||= 'student'
+    self.role ||= "student"
   end
 end
