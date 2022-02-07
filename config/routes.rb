b288cf1d88ccc913825ac90ea1ad7550e6e00406
brandaoplaster <brandaoplaster@gmail.com>
2022-02-06 21:15:17 -0300

    refactor: Changing the topic and post structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :topics
-  devise_for :users, controllers: { registrations: 'registration'}
+  resources :topics do
+    scope module: :topics do
+      resources :posts
+    end
+  end
+
+  devise_for :users, controllers: { registrations: "registration" }
 end
